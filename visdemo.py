fa230c950e4c355cb737a20693edc3602758c528
GoldenPotatis <chang_su0313@126.com>
2020-09-08 22:12:27 +0200

    Update visdemo.py

--- visdemo.py
+++ visdemo.py
@@ -12,8 +12,7 @@ globe_file = './ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp'
 countries = gpd.read_file(globe_file)
 
 while name.title() not in countries['ADMIN'].values:
-    print('Please retype the correct name \
-    (perhaps the full name of the country).')
+    print('Please retype the correct name \n(perhaps the full name of the country).')
     name = input('Please type in the country name: ')
 
 geometry = countries[countries['ADMIN'] == name.title()]['geometry']
