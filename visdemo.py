494a0fdf4711fe9f0bf8aaf0f0c2313234c3fea3
GoldenPotatis <chang_su0313@126.com>
2020-09-08 21:43:00 +0200

    visdemo added

---
+++ visdemo.py
@@ -0,0 +1,24 @@
+# OSeMOSYS Visualization Demo
+# Chang Su
+# 2020-09-05
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+name = input('Please type in the country name: ')
+
+globe_file = './ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp'
+
+countries = gpd.read_file(globe_file)
+
+while name.title() not in countries['ADMIN'].values:
+    print('Please retype the correct name \
+    (perhaps the full name of the country).')
+    name = input('Please type in the country name: ')
+
+geometry = countries[countries['ADMIN'] == name.title()]['geometry']
+
+shape = geometry.plot()
+shape.set_axis_off()
+
+plt.show()
